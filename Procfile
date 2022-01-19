9ee0ad302eca6074ea6c6eb97e6015614f976c6f
arsha-123 <arshaashok850@gmail.com>
2022-01-19 15:33:58 +0530

    config changed

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn project10.wsgi --log-file -
--- project10/settings.py
+++ project10/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7*clq_-@#bp-kmo02ak9a9v*i7eklx!#zruy!y6kg&vsa9eig#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
