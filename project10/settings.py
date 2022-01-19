fb3fd3dd7b2d519fbd8631a7148ecd7a0b651ba8
arsha-123 <arshaashok850@gmail.com>
2022-01-19 15:46:37 +0530

    false changed to true

--- project10/settings.py
+++ project10/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7*clq_-@#bp-kmo02ak9a9v*i7eklx!#zruy!y6kg&vsa9eig#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = true
 
 ALLOWED_HOSTS = ['app10101010.herokuapp.com']
 
